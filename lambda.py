e499f4052b4f80bf5b0351a3898992fb63e41157
Jeremy <jeremyyong0101@gmail.com>
2024-01-04 13:50:02 +0800

    Update handler function

--- lambda.py
+++ lambda.py
@@ -1,3 +1,18 @@
+import json
+import os
+import requests
+
 def handler(event, context):
-    print("Hello")
-    return None
+    # TODO implement
+    BOT_TOKEN = os.environ.get('TOKEN')
+    BOT_CHAT_ID = os.environ.get('CHATID')
+    #BOT_CHAT_ID = chat_id
+    bot_message = "Hello, this is the bot from telegram"
+    send_text = 'https://api.telegram.org/bot' + BOT_TOKEN + '/sendMessage?chat_id=' + BOT_CHAT_ID + \
+                '&parse_mode=HTML&text=' + bot_message
+    response = requests.get(send_text)
+    print(response)    
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Hello from Lambda!')
+    }
