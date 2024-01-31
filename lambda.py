d168adcef0ccd1600a4b2c5676a8cc9982c5b955
Jeremy <jeremyyong0101@gmail.com>
2024-01-31 18:16:57 +0800

    Abstract messaging function

--- lambda.py
+++ lambda.py
@@ -5,22 +5,23 @@ import psycopg2
 import urllib.parse as up
 from psycopg2 import Error
 
-help_message = "- Type /bookings to view all bookings for the next 7 days.\n- Type /book to get started with booking the buttery."
+
+BOT_TOKEN = os.environ.get('TOKEN')
+HELP_MESSAGE = "- Type /bookings to view all bookings for the next 7 days.\n- Type /book to get started with booking the buttery."
 
 def handler(event, context):
+    print(event)
     request_body = json.loads(event['body']) # Extract the Body from the call
-    chat_id = json.dumps(request_body['message']['chat']['id']) # Extract the chat id from message
+    BOT_CHAT_ID = json.dumps(request_body['message']['chat']['id']) # Extract the chat id from message
     command = json.dumps(request_body['message']['text']).strip('"') # Extract the text from the message
 
-    BOT_TOKEN = os.environ.get('TOKEN')
-    BOT_CHAT_ID = chat_id # Updating the Bot Chat Id to be dynamic instead of static one earlier
     command = command[1:] # Valid input command is /start or /help. however stripping the '/' here as it was having some conflict in execution.
     
     if command == 'start':
-        message = "Welcome to ButteryBot!\n\n" + help_message
+        send_message(BOT_TOKEN, BOT_CHAT_ID, "Welcome to ButteryBot!\n\n" + HELP_MESSAGE)
         
     elif command == 'help':
-        message = help_message
+        send_message(BOT_TOKEN, BOT_CHAT_ID, HELP_MESSAGE)
 
     # elif command == 'nigga':
     #     db_connect()
@@ -28,21 +29,13 @@ def handler(event, context):
     # elif command == 'bookings':
     #     conn = db_connect()
 
-    #     if not conn:
-    #         send_error_message("Sorry, there was an error ")
-
     # elif command == 'book':
     
     else:
-        message = "I'm sorry, I didn't understand that command. Please try again."
-
-    send_text = 'https://api.telegram.org/bot' + BOT_TOKEN + '/sendMessage?chat_id=' + BOT_CHAT_ID + '\&parse_mode=HTML&text=' + message
-    response = requests.get(send_text)
+        send_message(BOT_TOKEN, BOT_CHAT_ID, "I'm sorry, I didn't understand that command. Please try again.")
     
     return {
-        'statusCode': 200,
-        'method': "sendMessage",
-        'text': "This is from the return value"
+        'statusCode': 200
     }
 
 def db_connect():
@@ -53,3 +46,6 @@ def db_connect():
         return conn
     except Error as e:
         return False
+    
+def send_message(bot_token, chat_id, message):
+    requests.get('https://api.telegram.org/bot' + bot_token + '/sendMessage?chat_id=' + chat_id + '\&parse_mode=HTML&text=' + message)
