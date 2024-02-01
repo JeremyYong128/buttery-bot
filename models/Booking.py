326c4be41e4f53ba65bba1489a690386501b3369
Jeremy <jeremyyong0101@gmail.com>
2024-02-01 19:06:50 +0800

    Bug fix

--- models/Booking.py
+++ models/Booking.py
@@ -5,6 +5,9 @@ from datetime import date
 class Booking:
     time_format_string = "%-I:%M %p"
     date_format_string = "%a, %-d %b"
+
+    def calculate_end_time(start_time, duration):
+        return datetime.combine(date.today(), start_time) + timedelta(duration).time()
         
     def __init__(self, telegram_handle, date, start_time, duration):
         self.telegram_handle = telegram_handle
@@ -20,5 +23,4 @@ class Booking:
 
         return date_str + ": " + start_time_str + " to " + end_time_str
 
-    def calculate_end_time(start_time, duration):
-        return datetime.combine(date.today(), start_time) + timedelta(duration).time()
+    
