3534758021a8942e4960f6335194a1427cc73309
Jeremy <jeremyyong0101@gmail.com>
2024-02-21 21:21:22 +0800

    Add changes to message.py

--- message.py
+++ message.py
@@ -7,34 +7,23 @@ CONTACT_MESSAGE = "For more assistance, contact @carinateh, @jemmacheah, @jeremy
 UNKNOWN_COMMAND_MESSAGE = "Unknown command. Please ensure you have entered the correct command."
 NO_BOOKINGS_MESSAGE = "There are no bookings."
 
-class Message:
-    def with_chat_id(self, chat_id):
-        self.chat_id = chat_id
-        return self
+def send(chat_id, text, markup=None):
+    request_string = REQUEST_STRING + 'sendMessage?chat_id=' + chat_id + '&text=' + text
+    if markup:
+        request_string += '&reply_markup=' + self.markup
+    requests.get(request_string)
 
-    def with_text(self, text):
-        self.text = text
-        return self
+def send_start(chat_id):
+    send(chat_id, WELCOME_MESSAGE + "\n\n" + HELP_MESSAGE)
 
-    @staticmethod
-    def send(chat_id, text, markup=None):
-        request_string = REQUEST_STRING + 'sendMessage?chat_id=' + chat_id + '&text=' + text
-        if markup:
-            request_string += '&reply_markup=' + self.markup
-        requests.get(request_string)
+def send_help(chat_id):
+    send(chat_id, HELP_MESSAGE + "\n\n" + CONTACT_MESSAGE)
 
-    @staticmethod
-    def send_start(chat_id):
-        Message.send(chat_id, WELCOME_MESSAGE + "\n\n" + HELP_MESSAGE)
+def send_unknown_command(chat_id):
+    send(chat_id, UNKNOWN_COMMAND_MESSAGE)
 
-    @staticmethod
-    def send_help(chat_id):
-        Message.send(chat_id, HELP_MESSAGE + "\n\n" + CONTACT_MESSAGE)
-    
-    @staticmethod
-    def send_no_bookings(chat_id):
-        Message.send(chat_id, NO_BOOKINGS_MESSAGE)
-    
-    @staticmethod
-    def send_booking_dates(self):
-        r
+def send_no_bookings(chat_id):
+    send(chat_id, NO_BOOKINGS_MESSAGE)
+
+def send_booking_dates():
+    return
